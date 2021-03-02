bcb1d556b8d573c504dfddd5aa8cd7fcf46366f1
dak2 <dawt2h@gmail.com>
2021-03-03 00:10:44 +0900

    fix q 100

--- questions/question51_100/q_100.js
+++ questions/question51_100/q_100.js
@@ -1,1 +1,22 @@
-// ['a','b','c’]を{0: 'a’, 1: 'b’, 2: 'c'}にしてください
+// ['a','b','c']を{ 0: 'a’, 1: 'b’, 2: 'c' }にしてください
+
+// 解答
+const strArray = ['a','b','c'];
+Object.assign({}, strArray);
+
+// 別解
+const obj = {}
+strArray.forEach((str, index) => {
+  obj[index] = str
+})
+
+// 答え
+function toObject(ary){
+  const obj = {};
+  for(let i = 0; i < ary.length; i++){
+    obj[i] = ary[i];
+  }
+  return obj
+}
+
+toObject(strArray);
