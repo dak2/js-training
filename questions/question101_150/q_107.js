45437fa95a7066ba7d4b079a61939c141a50437b
dak2 <dawt2h@gmail.com>
2021-03-14 00:19:54 +0900

    add q_107

--- questions/question101_150/q_107.js
+++ questions/question101_150/q_107.js
@@ -1,6 +1,28 @@
-getAsync("fileA.txt", (error, result) => {
-  if (error) { // 取得失敗時の処理
-      throw error;
-  }
-  // 取得成功の処理
-});
+// 下記のPromise.allは任意のリクエストが失敗した場合、Promise.all はエラーで reject し、他のすべてのリクエストの結果を失います
+// 行 (*) で配列 responses がフェッチに成功したレスポンスオブジェクトと、失敗したエラーオブジェクトを含むようにコードを修正してください。
+
+let urls = [
+  'https://api.github.com/users/iliakan',
+  'https://api.github.com/users/remydadsadasadadaaa',
+  'https://api.github.com/users/jeresig'
+];
+
+Promise.all(urls.map(url => fetch(url)))
+  // 各レスポンスに対し、そのステータスを表示
+  .then(responses => { // (*)
+    for(let response of responses) {
+      alert(`${response.url}: ${response.status}`);
+    }
+  })
+
+
+// 解答
+Promise.all(urls.map(url => fetch(url).catch(err => err)))
+  // catchを定義しておけばエラーならエラーオブジェクト
+  // エラーでないならレスポンスオブジェクトとして返る
+  .then(responses => {
+    for(let response of responses) {
+      alert(`${response.url}: ${response.status}`);
+    }
+  })
+
